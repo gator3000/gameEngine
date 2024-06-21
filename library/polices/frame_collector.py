dd65501f3f1401ebcd8178434b9fc969e7ba1ed2
Gator3000 <aelian.brd@proton.me>
2024-06-21 16:33:30 +0100

    main_folder_creator.py work on windows

--- library/polices/frame_collector.py
+++ library/polices/frame_collector.py
@@ -3,10 +3,10 @@ from shutil import *
 
 def copy_police(target:str, name:str="pixels"):
     try:
-        folder = "/home/gator/dev/python/gameEngine/library/polices/"+name+"/"
+        i=0
+        folder = "/".join(os.getcwd().split("\\")) + "/" + name +"/"
         files = os.listdir(folder)
 
-        i=0
         for file in files:
             copy(folder+file, target)
             i+=1
